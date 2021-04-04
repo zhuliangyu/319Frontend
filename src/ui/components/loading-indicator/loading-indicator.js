3f9e24b3c330c7feff62e75cd68a57a4f5746424
mandywan <mandygwan@gmail.com>
2021-04-03 18:38:57 -0700

    ae loading indicator

--- src/ui/components/loading-indicator/loading-indicator.js
+++ src/ui/components/loading-indicator/loading-indicator.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import loaderSvg from '../../../assets/3-dot-loader.svg';
+import aeSvg from '../../../assets/ae-svg.svg';
 
 const LoadingIndicator = () => {
     
     return (
         <div className='loader'>
-            <img src={loaderSvg} width='100' height='100'></img>
+            <div className='loader-svg-container'>
+                <img src={aeSvg} width='300' height='100'></img>
+            </div>
+            <div className='loader-svg-container'>
+                <img src={loaderSvg} width='300' height='80'></img>
+            </div>
         </div>
     )
 }
